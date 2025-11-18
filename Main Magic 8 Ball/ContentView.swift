f79a063d67848a2a89e78c2e482f0af273c6fa6e
Edwin Tovar <poohwinnie2800@gmail.com>
2025-11-18 15:29:17 -0800

    Initial Commit

---
+++ Main Magic 8 Ball/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Main Magic 8 Ball
+//
+//  Created by Edwin Tovar on 11/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Main Magic 8 Ball/Main_Magic_8_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Main_Magic_8_BallApp.swift
+//  Main Magic 8 Ball
+//
+//  Created by Edwin Tovar on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Main_Magic_8_BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
